39dd46f678674f77e9ed782617657348234d33a1
matts1999 <matts1999@live.seminolestate.edu>
2024-05-28 23:15:18 -0400

    Add Javadoc comments to Fibonacci method

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,8 @@
 package cen4802;
 
+/**
+ * This class provides a method that calculates the nth term of the Fibonacci sequence.
+ */
 public class MainClass {
     public static void main(String[] args) {
         int n = 10;
@@ -7,6 +10,13 @@ public class MainClass {
         System.out.println("The " + n + "th term of the sequence is " + result);
     }
 
+    /**
+     * Returns the nth term of the sequence.
+     *
+     * @param n the position in the sequence
+     * @return the nth term of the sequence
+     */
+    
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
